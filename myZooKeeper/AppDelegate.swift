65a9b6cdc79f2f263d397f5e60c49fd452d8a541
Sandy <sandyvasquez9@gmail.com>
2021-02-15 23:16:41 -0800

    added tab bar icons

--- myZooKeeper/AppDelegate.swift
+++ myZooKeeper/AppDelegate.swift
@@ -15,9 +15,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
   func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions:
         [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+
         // Override point for customization after application launch.
-    
-        FirebaseApp.configure()
+
+    FirebaseApp.configure()
         db = Firestore.firestore()
         GIDSignIn.sharedInstance().clientID = FirebaseApp.app()?.options.clientID
         return true
